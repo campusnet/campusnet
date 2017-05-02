6ccb1c52d6daf81c2a4049fe2734184cf5eb70c6
Jilvin <jilvinjacob@gmail.com>
2017-05-02 21:43:07 +0530

    partial installation - without frontend complete

--- dist/config/routes.js
+++ dist/config/routes.js
@@ -8,32 +8,70 @@
  * https://sailsjs.com/anatomy/config/routes-js
  */
 
-module.exports.routes = {
+installationFlag=0;
+
+//check for campusnetConfig.js - start
 
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
-
-  '/': {
-    view: 'homepage'
-  },
+if(installationFlag==1)
+{
+  module.exports.routes = {
 
 
+    /***************************************************************************
+    *                                                                          *
+    * Make the view located at `views/homepage.ejs` your home page.            *
+    *                                                                          *
+    * (Alternatively, remove this and add an `index.html` file in your         *
+    * `assets` directory)                                                      *
+    *                                                                          *
+    ***************************************************************************/
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
+    '/': {
+      view: 'homepage'
+    },
 
-};
+
+
+    /***************************************************************************
+    *                                                                          *
+    * Custom routes here...                                                    *
+    *                                                                          *
+    * If a request to a URL doesn't match any of the custom routes above, it   *
+    * is matched against Sails route blueprints. See `config/blueprints.js`    *
+    * for configuration options and examples.                                  *
+    *                                                                          *
+    ***************************************************************************/
+
+  };
+}
+else {
+  module.exports.routes = {
+
+
+    /***************************************************************************
+    *                                                                          *
+    * Make the view located at `views/homepage.ejs` your home page.            *
+    *                                                                          *
+    * (Alternatively, remove this and add an `index.html` file in your         *
+    * `assets` directory)                                                      *
+    *                                                                          *
+    ***************************************************************************/
+
+    '/install': 'campusnetInstallation.start',
+    '/install/write': 'campusnetInstallation.write',
+    '/' : '/install'
+
+
+    /***************************************************************************
+    *                                                                          *
+    * Custom routes here...                                                    *
+    *                                                                          *
+    * If a request to a URL doesn't match any of the custom routes above, it   *
+    * is matched against Sails route blueprints. See `config/blueprints.js`    *
+    * for configuration options and examples.                                  *
+    *                                                                          *
+    ***************************************************************************/
+
+  };
+}
