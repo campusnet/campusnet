0e89584578ab62bd2117e98cd997fb0dd626a773
Jilvin <jilvinjacob@gmail.com>
2017-11-09 20:07:02 +0530

    working on facebook login

--- frontend/cetian/app/src/main/java/cetian/campusnet/cetian/MainActivity.java
+++ frontend/cetian/app/src/main/java/cetian/campusnet/cetian/MainActivity.java
@@ -1,17 +1,59 @@
 package cetian.campusnet.cetian;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.facebook.CallbackManager;
+import com.facebook.CallbackManager.Factory;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
 public class MainActivity extends AppCompatActivity {
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        // Make sure this is before calling super.onCreate
+//        setTheme(R.style.HiddenTitleAppTheme);
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+
+    CallbackManager callbackManager = Factory.create();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // Make sure this is before calling super.onCreate
+    public void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.HiddenTitleAppTheme);
         super.onCreate(savedInstanceState);
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
